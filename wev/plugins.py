295148752bdd66fef72600aad7a2fb8f46e5e175
Cariad Eccleston <cariad@cariad.me>
2020-12-05 15:09:40 +0000

    WIP

--- wev/plugins.py
+++ wev/plugins.py
@@ -1,15 +1,32 @@
-from typing import cast
+from typing import Any, Dict, cast
 
 from pkg_resources import iter_entry_points
 
-from wev.exceptions import HandlerNotInstalled
+from wev.exceptions import MultiplePluginsError, NoPluginError
 from wev.sdk import PluginBase
-from wev.variable import Variable
 
 
-def get_plugin(variable: Variable) -> PluginBase:
-    for impl in iter_entry_points("wev.plugins"):
-        if impl.name == variable.handler:
-            return cast(PluginBase, impl.load().Plugin(config=variable.configuration))
-    else:
-        raise HandlerNotInstalled(name=variable.handler)
+def get_plugin(handler: str, configuration: Dict[Any, Any]) -> PluginBase:
+    """
+    Gets a plugin instance for the given handler.
+
+    Args:
+        handler:       Handler to get a plugin for.
+        configuration: Configuration to pass to the plugin.
+
+    Returns:
+        Plugin instance.
+
+    Raises:
+        NoPluginError:        No plugins installed for this handler.
+        MultiplePluginsError: Multple plugins installed for this handler.
+    """
+    plugins = [e for e in iter_entry_points("wev.plugins") if e.name == handler]
+
+    if len(plugins) == 0:
+        raise NoPluginError(handler=handler)
+
+    if len(plugins) > 1:
+        raise MultiplePluginsError(handler=handler, count=len(plugins))
+
+    return cast(PluginBase, plugins[0].load().Plugin(configuration))
