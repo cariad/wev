1b7d039f31f5ba23a16a1db9e6a9c00e727ecd69
Cariad Eccleston <cariad@cariad.me>
2020-12-05 11:26:02 +0000

    WIP

--- wev/cli.py
+++ wev/cli.py
@@ -1,6 +1,8 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from logging import getLogger
 from typing import List, Optional
+
+from colorama import Style
 
 from wev.executor import execute
 from wev.explainer import explain
@@ -13,18 +15,66 @@ class CLI:
     CLI executor.
 
     Arguments:
-        args (List[str]): Optional arguments. Will read from the command line if
-                          omitted. Intended for tests.
+        args: Arguments. Will read from the command line if omitted.
     """
 
     def __init__(self, args: Optional[List[str]] = None) -> None:
         init()
-        self.logger = getLogger()
+        self.logger = getLogger("wev")
+
+        wev = Style.BRIGHT + "wev" + Style.RESET_ALL
+
+        with_env_vars = (
+            Style.BRIGHT
+            + "w"
+            + Style.RESET_ALL
+            + "ith "
+            + Style.BRIGHT
+            + "e"
+            + Style.RESET_ALL
+            + "nvironment "
+            + Style.BRIGHT
+            + "v"
+            + Style.RESET_ALL
+            + "ariables"
+        )
+
+        description = f"{wev} runs shell commands {with_env_vars}."
+
+        epilog = (
+            "examples:\n"
+            + "  wev --explain\n"
+            + "  wev --version\n"
+            + "  wev pipenv install\n"
+            + "  wev --log-level DEBUG pipenv install\n"
+            + "\n"
+            + "Built with ❤️ by Cariad Eccleston: https://github.com/cariad/wev"
+        )
 
         self.arg_parser = ArgumentParser(
             "wev",
-            description="`wev` runs shell commands With Environment Variables..",
-            epilog="Homepage: https://github.com/cariad/wev",
+            description=description,
+            epilog=epilog,
+            formatter_class=RawDescriptionHelpFormatter,
+        )
+
+        self.arg_parser.add_argument(
+            "command",
+            nargs="...",
+            help=f"shell command and arguments to run {with_env_vars}.",
+        )
+
+        self.arg_parser.add_argument(
+            "--explain",
+            action="store_true",
+            help=f"explain what {wev} will do",
+        )
+
+        self.arg_parser.add_argument(
+            "--log-level",
+            default="INFO",
+            help="log level (default is INFO)",
+            metavar="LEVEL",
         )
 
         self.arg_parser.add_argument(
@@ -32,36 +82,37 @@ class CLI:
             action="store_true",
             help="print the version",
         )
-        self.arg_parser.add_argument(
-            "--explain",
-            action="store_true",
-            help="explain what `wev` will do",
-        )
-        self.arg_parser.add_argument("command", nargs="...")
-        self.arg_parser.add_argument("--log-level", default="INFO")
+
         self.args = self.arg_parser.parse_args(args)
         set_level(self.args.log_level)
 
-    def try_invoke(self) -> int:
-        if self.args.command:
-            return execute(command=self.args.command)
-        elif self.args.explain:
-            explain()
-        elif self.args.version:
-            self.logger.info(get_version())
-        else:
-            self.arg_parser.print_help()
-        return 0
-
     def invoke(self) -> int:
         """
-        Invokes the apppriate task for the given command line arguments.
+        Invokes the appropriate task for the given command line arguments
 
         Returns:
-            int: Shell return code.
+            Shell return code.
         """
         try:
             return self.try_invoke()
         except Exception as ex:
             self.logger.critical(ex)
             return 1
+
+    def try_invoke(self) -> int:
+        """
+        Attempts to invokes the appropriate task for the given command line
+        arguments. Could raise any exceptions.
+
+        Returns:
+            Shell return code.
+        """
+        if self.args.command:
+            return execute(command=self.args.command)
+        elif self.args.explain:
+            explain()
+        elif self.args.version:
+            print(get_version())
+        else:
+            self.arg_parser.print_help()
+        return 0
--- wev/test_cli.py
+++ wev/test_cli.py
@@ -1,98 +1,46 @@
-# from mock import Mock, patch
+from mock import Mock, patch
 
-# from wev.cli import CLI
-
-
-# def test_args() -> None:
-#     assert not CLI().args.command
-#     assert not CLI().args.info
-#     assert not CLI().args.profile
-#     assert not CLI().args.version
+from wev.cli import CLI
 
 
-# def test_args__command() -> None:
-#     assert CLI(["pipenv", "sync"]).args.command == ["pipenv", "sync"]
+def test_args() -> None:
+    assert not CLI().args.command
+    assert not CLI().args.explain
+    assert not CLI().args.version
+    assert CLI().args.log_level == "INFO"
 
 
-# def test_args__info() -> None:
-#     assert CLI(["--info"]).args.info
+@patch("wev.cli.execute", return_value=0)
+def test_command(execute: Mock) -> None:
+    cli = CLI(["pipenv", "sync"])
+    assert cli.args.command == ["pipenv", "sync"]
+    assert cli.invoke() == 0
+    execute.assert_called_with(command=["pipenv", "sync"])
 
 
-# def test_args__profile() -> None:
-#     assert CLI(["--profile", "foo"]).args.profile == "foo"
+@patch("wev.cli.execute", side_effect=Exception())
+def test_command__fails(execute: Mock) -> None:
+    assert CLI(["pipenv", "sync"]).invoke() == 1
 
 
-# def test_args__version() -> None:
-#     assert CLI(["--version"]).args.version
+@patch("wev.cli.explain")
+def test_args__explain(explain: Mock) -> None:
+    cli = CLI(["--explain"])
+    assert cli.args.explain
+    assert cli.invoke() == 0
+    explain.assert_called_with()
 
 
-# @patch("corf.cli.Executor")
-# def test_execute__success(executor_maker: Mock) -> None:
-#     executor = Mock()
-#     executor.execute.return_value = 0
-#     executor_maker.return_value = executor
-#     assert CLI().execute() == 0
-#     assert executor_maker.called_with()
-#     assert executor.execute.called_with()
+@patch("wev.cli.get_version")
+def test_args__version(get_version: Mock) -> None:
+    cli = CLI(["--version"])
+    assert cli.args.version
+    assert cli.invoke() == 0
+    get_version.assert_called_with()
 
 
-# @patch("corf.cli.Executor")
-# def test_execute__fail(executor_maker: Mock) -> None:
-#     executor = Mock()
-#     executor.execute.side_effect = Exception("printer on fire")
-#     executor_maker.return_value = executor
-#     assert CLI().execute() == 1
-
-
-# @patch("corf.cli.CLI.print_help", return_value=0)
-# @patch("corf.cli.CLI.setup_logging")
-# def test_invoke(setup_logging: Mock, print_help: Mock) -> None:
-#     assert CLI([]).invoke() == 0
-#     setup_logging.assert_called_with()
-#     print_help.assert_called_with()
-
-
-# @patch("corf.cli.CLI.execute", return_value=0)
-# def test_invoke__command(execute: Mock) -> None:
-#     assert CLI(["pipenv", "sync"]).invoke() == 0
-#     execute.assert_called_with()
-
-
-# @patch("corf.cli.CLI.print_info", return_value=0)
-# def test_invoke__info(print_info: Mock) -> None:
-#     assert CLI(["--info"]).invoke() == 0
-#     print_info.assert_called_with()
-
-
-# @patch("corf.cli.CLI.print_version", return_value=0)
-# def test_invoke__version(print_version: Mock) -> None:
-#     assert CLI(["--version"]).invoke() == 0
-#     print_version.assert_called_with()
-
-
-# def test_make_arg_parser() -> None:
-#     assert CLI().make_arg_parser()
-
-
-# def test_print_help() -> None:
-#     assert CLI().print_help() == 0
-
-
-# def test_print_info() -> None:
-#     assert CLI().print_info() == 0
-
-
-# def test_print_version() -> None:
-#     assert CLI().print_version() == 0
-
-
-# @patch("corf.cli.getLogger")
-# def test_setup_logging__boto(get_logger: Mock) -> None:
-#     CLI().setup_logging()
-#     get_logger.assert_called_with("boto")
-#     get_logger.return_value.setLevel.assert_called_with("CRITICAL")
-
-
-# def test_setup_logging__succeeds() -> None:
-#     CLI().setup_logging()
-#     assert True
+@patch("wev.cli.ArgumentParser.print_help")
+def test_args__help(print_help: Mock) -> None:
+    cli = CLI([])
+    assert cli.invoke() == 0
+    print_help.assert_called_with()
