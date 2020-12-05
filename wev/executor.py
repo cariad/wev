9272bfb8ca7f247bb26ba004ebffc55b7b59dc28
Cariad Eccleston <cariad@cariad.me>
2020-12-05 11:47:06 +0000

    WIP

--- wev/executor.py
+++ wev/executor.py
@@ -8,6 +8,15 @@ from wev.resolver import resolve
 
 
 def execute(command: List[str]) -> int:
+    """
+    Executes the command with environment variables.
+
+    Args:
+        command: Shell command.
+
+    Returns:
+        Shell return code.
+    """
     logger = getLogger("wev")
     variables = resolve()
     logger.info("Starting %s%s%s...", Style.BRIGHT, command[0], Style.RESET_ALL)
