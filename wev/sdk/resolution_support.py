b102942c14919197207f7904533f9c2a9745dbfb
Cariad Eccleston <cariad@hey.com>
2021-01-16 15:52:32 +0000

    Add types and documentation

--- wev/sdk/resolution_support.py
+++ wev/sdk/resolution_support.py
@@ -3,8 +3,27 @@ from typing import Callable
 
 
 class ResolutionSupport:
+    """
+    Helps plugins to resolve values.
+
+    Arguments:
+        confidential_prompt: A function for requesting runtime confidential
+                             information from the user.
+
+                             The first argument prescribes a short preamble, and
+                             the second is the short prompt to print next to the
+                             input cursor.
+
+                             The function returns the user-entered string.
+
+        logger:              Logger. Plugins should use this rather than
+                             instantiate their own.
+    """
+
     def __init__(
-        self, logger: Logger, confidential_prompt: Callable[[str, str], str]
+        self,
+        confidential_prompt: Callable[[str, str], str],
+        logger: Logger,
     ) -> None:
-        self.logger = logger
-        self.confidential_prompt = confidential_prompt
+        self.logger: Logger = logger
+        self.confidential_prompt: Callable[[str, str], str] = confidential_prompt
